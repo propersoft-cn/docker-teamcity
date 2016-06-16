dcfdfa673094631d20b35dc53aef75efc2bd9964
AlphaHinex <AlphaHinex@gmail.com>
2016-06-16 13:32:29 +0800

    Install plugin in entry point

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 set -e
 
-mkdir -p $TEAMCITY_DATA_PATH/lib/jdbc $TEAMCITY_DATA_PATH/config
+mkdir -p $TEAMCITY_DATA_PATH/lib/jdbc $TEAMCITY_DATA_PATH/config $TEAMCITY_DATA_PATH/plugins
+
 if [ ! -f "$TEAMCITY_DATA_PATH/lib/jdbc/postgresql-9.3-1103.jdbc41.jar" ];
 then
     echo "Downloading postgress JDBC driver..."
     wget -P $TEAMCITY_DATA_PATH/lib/jdbc https://jdbc.postgresql.org/download/postgresql-9.3-1103.jdbc41.jar
+fi
+
+if [ ! -f "$TEAMCITY_DATA_PATH/plugins/teamcity.github.zip" ];
+then
+    echo "Downloading TeamCity.GitHub plugin..."
+    wget -P $TEAMCITY_DATA_PATH/plugins http://teamcity.jetbrains.com/guestAuth/repository/download/bt398/lastest.lastSuccessful/teamcity.github.zip
 fi
 
 # Move ROOT to use custom context
