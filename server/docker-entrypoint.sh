51898596a9dd5fbec55f8d1604d1b412d55b9e48
AlphaHinex <AlphaHinex@gmail.com>
2016-07-25 17:52:56 +0800

    Fix a bug

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -15,8 +15,11 @@ then
     wget -P $TEAMCITY_DATA_PATH/plugins http://teamcity.jetbrains.com/guestAuth/repository/download/bt398/lastest.lastSuccessful/teamcity.github.zip
 fi
 
-# Move ROOT to use custom context
-mv /opt/TeamCity/webapps/ROOT /opt/TeamCity/webapps/$TEAMCITY_CONTEXT
+if [ -d "/opt/TeamCity/webapps/ROOT" ];
+then
+    # Move ROOT to use custom context
+    mv /opt/TeamCity/webapps/ROOT /opt/TeamCity/webapps/$TEAMCITY_CONTEXT
+fi
 
 echo "Starting teamcity..."
 exec /opt/TeamCity/bin/teamcity-server.sh run
