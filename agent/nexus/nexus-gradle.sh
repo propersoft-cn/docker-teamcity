5abc363db84b6d5105bacd616d3509b8316dc420
AlphaHinex <AlphaHinex@gmail.com>
2016-09-22 11:00:36 +0800

    Need to mkdir (maybe)

--- agent/nexus/nexus-gradle.sh
+++ agent/nexus/nexus-gradle.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+mkdir -p /home/teamcity/.gradle
 echo 'nexusUrl=https://server.propersoft.cn:8081
 nexusUsername=NEXUS_USER
 nexusPassword=NEXUS_PWD' > /home/teamcity/.gradle/gradle.properties
