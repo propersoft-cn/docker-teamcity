174de979848eb759d8ebdedea7c2c64d6db06d5c
AlphaHinex <AlphaHinex@gmail.com>
2017-02-04 11:04:44 +0800

    Try to install ionic

---
+++ agent/ionic/nexus/nexus-gradle.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+mkdir -p /home/teamcity/.gradle
+echo 'nexusUrl=https://server.propersoft.cn:8081
+nexusUsername=NEXUS_USER
+nexusPassword=NEXUS_PWD
+org.gradle.jvmargs=-Xmx2048M' > /home/teamcity/.gradle/gradle.properties
+sed -i "s/NEXUS_USER/$NEXUS_USER/g" /home/teamcity/.gradle/gradle.properties
+sed -i "s/NEXUS_PWD/$NEXUS_PWD/g" /home/teamcity/.gradle/gradle.properties
