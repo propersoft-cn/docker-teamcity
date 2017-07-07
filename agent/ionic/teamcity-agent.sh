17da0890331cdaaa7aa17d716419a8c5cfa326e9
AlphaHinex <AlphaHinex@gmail.com>
2017-07-07 17:39:34 +0800

    Clean code

--- agent/ionic/teamcity-agent.sh
+++ agent/ionic/teamcity-agent.sh
@@ -24,7 +24,7 @@ fi
 
 # if we have "--link some-docker:docker" and not DOCKER_HOST, let's set DOCKER_HOST automatically
 if [ -z "$DOCKER_HOST" -a "$DOCKER_PORT_2375_TCP" ]; then
-	export DOCKER_HOST="$DOCKER_PORT_2375_TCP"
+    export DOCKER_HOST="$DOCKER_PORT_2375_TCP"
 fi
 
 cd %AGENT_DIR%
